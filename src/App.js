d2318740ce147e30bac299242f291367a9858548
yasminmilhomem <yasminmilhomem1517@gmail.com>
2022-09-09 11:02:37 -0300

    Contador com Contexto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 import { HandleCounter } from './components/handleCounter';
 import { DoubleCounter } from './components/doubleCounter';
+import { CounterContextProvider } from './contexts/CounterContext';
+
 
 function App () {
-  const [counter, setCounter] = useState(0);
   return(
-    <div>
-      <HandleCounter counter={counter} setCounter={setCounter}/>
-      <DoubleCounter counter={counter}/>
-    </div>
+    //como o CounterContext é um componente pai do HandleCounter e do DoubleCounter
+    //ele pode armazenar um estado que será compartilhado entre eles
+    //a função do contexto é armazenar estados e outras coisas que serão compartilhadas entre os componentes
+    //e isso não é função de um componente!
+
+    <CounterContextProvider>
+      <div>
+        <HandleCounter />
+        <DoubleCounter />
+      </div>
+    </CounterContextProvider>
   );
 }
 
